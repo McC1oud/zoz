3f155d2dacdf8a06f5d001d98c6bf5922cc6858b
McC1oud <neon__fox@hotmail.com>
2018-06-13 17:53:13 +1000

    Project reset

---
+++ NetworkedChatWindow/Assets/ServerData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerData : NetworkBehaviour {
+
+    public string currentMessage;
+
+	public void ServerMessages(string newMessage)
+    {
+        currentMessage = newMessage;
+
+        //RpcCastNewMessage(currentMessage);
+    }
+/*
+    [ClientRpc]
+    public void RpcCastNewMessage(string theMessage)
+    {
+        transform.GetComponents<TextManagerHandler>.ServerMessages(). = theMessage;
+    }
+    */
+}
---
+++ NetworkedChatWindow/Assets/TextManagerHandler.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class TextManagerHandler : NetworkBehaviour {
+
+    public int maxMessages = 100;
+
+    public GameObject serverReference;
+
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+
+
+	void Start ()
+    {
+        if(isServer)
+        {
+            return;
+        }
+
+		if(!isLocalPlayer)
+        {
+            Destroy(this);
+            return;
+        }
+	}
+	
+
+	void Update ()
+    {
+        if (chatBox.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessageToChat(chatBox.text);
+                chatBox.text = "";
+            }
+        }
+        /*
+        else
+        {
+            if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+            {
+                chatBox.ActivateInputField();
+            }
+        }
+           
+
+        if(!chatBox.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //SendMessageToChat("You pressed space!");
+                //Debug.Log("Space");
+            }
+        }
+
+		*/
+	}
+
+    public void SendMessageToChat(string text)
+    {
+        if(messageList.Count >= maxMessages)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+
+        newMessage.textObject.text = "Nelson: " + newMessage.text;
+
+        serverReference.transform.GetComponent<ServerData>().ServerMessages(newMessage.textObject.text);
+
+        newMessage.textObject.text = serverReference.transform.GetComponent<ServerData>().currentMessage;
+
+        messageList.Add(newMessage);
+
+
+    }
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textObject;
+}
